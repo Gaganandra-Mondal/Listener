5962116a474dbefa3a56b73a442ef251ec3926f7
Debottam Kar <debottamkar2003@gmail.com>
2025-09-23 23:30:37 +0530

    backend initial setup done

--- backend/controllers/homeHandler.js
+++ backend/controllers/homeHandler.js
@@ -3,8 +3,9 @@ import erroHandler from "../error.js"
 
 const homeHandler = async (req, res) => {
     try {
-        let { row } = await pool.query("select * from songs limit 10;")
-        res.status(200).json({ message: row });
+        let { rows } = await pool.query("select * from songs limit 10;");
+        // console.log(rows);
+        res.status(200).json({ message: rows });
     } catch (err) {
         console.log(err.message);
         erroHandler(res);
