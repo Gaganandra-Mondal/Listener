46482e8d9b6ed2c13ae822572b6d2c4ea11c08ab
Debottam Kar <debottamkar2003@gmail.com>
2025-09-24 12:13:46 +0530

    backend initial setup done

---
+++ backend/controllers/userHandler.js
@@ -0,0 +1,19 @@
+import pool from "../db.js"
+import erroHandler from "../error.js"
+
+const userHanlder = async (req, res) => {
+    try {
+        const id = req.userID;
+        const { rows } = await pool.query("select id, name, email from users where id = $1", [id]);
+        if (rows.length > 0) {
+            res.status(200).json({ message: rows });
+        } else {
+            res.status(400).json({ message: "Bad Request." });
+        }
+    } catch (err) {
+        console.log(err.message);
+        erroHandler(res);
+    }
+}
+
+export default userHanlder;
