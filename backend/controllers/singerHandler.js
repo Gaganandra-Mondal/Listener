2b3b17c8989825e532b2b208f215abc519a96158
Debottam Kar <debottamkar2003@gmail.com>
2025-09-24 13:13:19 +0530

    backend setup done

--- backend/controllers/singerHandler.js
+++ backend/controllers/singerHandler.js
@@ -1,11 +1,11 @@
-import errorHanlder from "../error";
-import pool from "../db";
+import errorHanlder from "../error.js";
+import pool from "../db.js";
 
-const singerHandler = async(req, res) => {
+const singerHandler = async (req, res) => {
     try {
         const id = req.userID;
-        const response = await pool.query('select email,name,gender,manager_email,manager_name from artists where id = $1;',[id]);
-        if(response.rows.length === 0){
+        const response = await pool.query('select email,name,gender,manager_email,manager_name from artists where id = $1;', [id]);
+        if (response.rows.length === 0) {
             return res.status(401).json({ message: "artist not found" });
         } else {
             res.status(200).json({ message: "artist's data fetched successfully" });
