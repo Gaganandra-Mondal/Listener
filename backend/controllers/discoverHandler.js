3cfcd60a4aab3e43ebc59c492c9bb86fe8745a4e
Debottam Kar <debottamkar2003@gmail.com>
2025-10-02 21:23:53 +0530

    working on frontend

--- backend/controllers/discoverHandler.js
+++ backend/controllers/discoverHandler.js
@@ -4,8 +4,8 @@ import errorHandler from '../error.js';
 const discoverHandler = async (req, res) => {
     try {
         // Logic : We'll fetch those songs from songs table that are not streamed by the user.
-        const userId = req.user.id;
-        const { rows } = await pool.query("SELECT * FROM songs WHERE id NOT IN (SELECT sid FROM streams WHERE uid=$1) ORDER BY RANDOM()", [userId]);
+        const userId = req.userID;
+        const { rows } = await pool.query("SELECT * FROM songs WHERE id NOT IN (SELECT sid FROM streamed WHERE uid=$1) ORDER BY RANDOM()", [userId]);
         if (rows.length === 0) {
             return res.status(400).json({ message: "No songs found" });
         } else {
--- backend/controllers/recomendedHandler.js
+++ backend/controllers/recomendedHandler.js
@@ -7,11 +7,11 @@ const recomendedHandler = async (req, res) => {
         if (!uid) {
             return res.status(401).json({ message: "Unauthorized" });
         } else {
-            const { rows } = await pool.query("SELECT s.id, s.name, s.url, s.lyrics, s.duration, s.genre, s.uploaded_on FROM songs s JOIN followers f ON s.aid = f.aid WHERE f.uid = $1 AND s.id NOT IN (SELECT lm.sid FROM liked_music lm WHERE lm.uid = $1);", [uid]);
+            const { rows } = await pool.query("SELECT s.id as id, s.name as sname,s.img as img, s.url as url, s.lyrics as lyrics, s.duration as duration, s.genre as genre, s.uploaded_on as uploaded_on FROM songs s JOIN followers f ON s.aid = f.aid WHERE f.uid = $1 AND s.id NOT IN (SELECT lm.sid FROM liked_music lm WHERE lm.uid = $1);", [uid]);
             if (rows.length === 0) {
                 return res.status(400).json({ message: "No songs found" });
             } else {
-            return res.status(200).json(rows);
+                return res.status(200).json({ message: rows });
             }
         }
     } catch (err) {
