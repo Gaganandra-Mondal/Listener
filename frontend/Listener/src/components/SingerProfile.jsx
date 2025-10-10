c608e4622780f18347ca5d630f7c615f508c7e44
Debottam Kar <debottamkar2003@gmail.com>
2025-10-11 02:17:37 +0530

    userProfile and singerProfile connection with backend is done

--- frontend/Listener/src/components/SingerProfile.jsx
+++ frontend/Listener/src/components/SingerProfile.jsx
@@ -1,7 +1,40 @@
-const SingerProfile = () => {
-  return (
-    <div>This is the SingerProfile</div>
-  )
-}
+import { useState, useEffect } from "react";
 
-export default SingerProfile
+const SingerProfile = () => {
+  let [singerData, setSingerData] = useState({
+    name: "Singer",
+    email: "singer@gmail.com",
+  });
+  useEffect(() => {
+    async function fecthSingerProfile() {
+      try {
+        let response = await fetch("http://localhost:3333/singerProfile", {
+          method: "GET",
+          credentials: "include",
+        });
+        if (response.ok) {
+          let data = await response.json();
+          // userProfile object will be set, data.message = {},(userProfile object)
+          setSingerData(data.message);
+        }
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+    let useType = localStorage.getItem("u_type");
+    // if userType is not undefined and userType is singer
+    if (useType && useType === "singer") {
+      fecthSingerProfile();
+    }
+  }, []);
+  return (
+    <div>
+      <div>
+        Name: {singerData.name} <br />
+        Email: {singerData.email}
+      </div>
+    </div>
+  );
+};
+
+export default SingerProfile;
--- frontend/Listener/src/components/UserProfile.jsx
+++ frontend/Listener/src/components/UserProfile.jsx
@@ -1,10 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const UserProfile = () => {
   let [userData, setUserData] = useState({
     name: "Listener",
     email: "listener@gmail.com",
   });
+  useEffect(() => {
+    async function fecthUserProfile() {
+      try {
+        let response = await fetch("http://localhost:3333/userProfile", {
+          method: "GET",
+          credentials: "include",
+        });
+        if (response.ok) {
+          let data = await response.json();
+          // userProfile object will be set, data.message = {},(userProfile object)
+          setUserData(data.message);
+        }
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+    let useType = localStorage.getItem("u_type");
+    if (useType && useType === "user") {
+      fecthUserProfile();
+    }
+  }, []);
   return (
     <div>
       <div>
